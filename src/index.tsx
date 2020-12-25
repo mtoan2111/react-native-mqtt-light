9f3ed0c1e368c2fb8871ae7959603416a1f6c98a
mtoan2111 <toan93.hust@gmail.com>
2020-12-25 10:11:03 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -23,7 +23,7 @@ class Mqtt {
 
     initListener = () => {
         try {
-            this.nativeEvent.addListener('message', (data) => {
+            this.nativeEvent.addListener('message', (data: any) => {
                 try {
                     typeof this.onmessage === 'function' &&
                         this.onmessage?.(data);
@@ -32,7 +32,7 @@ class Mqtt {
                 }
             });
 
-            this.nativeEvent.addListener('lostConnect', (data) => {
+            this.nativeEvent.addListener('lostConnect', (data: any) => {
                 try {
                     typeof this.onlostconnect === 'function' &&
                         this.onlostconnect?.(data);
@@ -43,7 +43,7 @@ class Mqtt {
                 }
             });
 
-            this.nativeEvent.addListener('error', (data) => {
+            this.nativeEvent.addListener('error', (data: any) => {
                 try {
                     this.sendError(
                         'MQTT/initListener/error: err => ' + data.message,
